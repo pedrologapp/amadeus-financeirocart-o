132f40c544f9c28e6a98c98ec2c243c7930db028
Pedro Luciano <128741246+pedrologapp@users.noreply.github.com>
2025-10-22 18:43:47 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -310,7 +310,8 @@ function App() {
       <section className="hero-section min-h-screen flex items-center justify-center text-white relative">
         <div className="text-center z-10 max-w-4xl mx-auto px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-6 animate-fade-in">
-            Amadeus - geração de Cobranças
+            EduHUB 
+			Geração de Cobranças
           </h1>
         
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
@@ -886,6 +887,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
